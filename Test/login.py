06c96b490016e97baf2aa7b75eb29588fd205ad8
PattyRosembluth <patricia.rosembluth@gmail.com>
2023-03-15 12:25:11 -0400

    Implementing Test class

--- Test/login.py
+++ Test/login.py
@@ -1,16 +1,30 @@
+import time
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = Options()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                          options=options)
 
-driver.get("https://www.saucedemo.com/")
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
+class LoginTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.options = Options()
+        cls.options.add_experimental_option("detach", True)
+        cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                      options=cls.options)
+
+    def test_valid_login(self):
+        self.driver.get("https://www.saucedemo.com/")
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "login-button").click()
+        time.sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
